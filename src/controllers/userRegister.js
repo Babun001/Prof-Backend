3f194b4b83ac455f09f0ab960792e91b50217015
Babun001 <babunroy987@gmail.com>
2024-12-20 23:50:32 +0530

    check api error class

--- src/controllers/userRegister.js
+++ src/controllers/userRegister.js
@@ -1,9 +1,14 @@
 import {asyncAwaitHandler} from "../utilities/asyncFuncHandler.js";
-
+import apiError from "../utilities/apiErrorsHandler.js"
 const userRegister = asyncAwaitHandler(async(req, res) =>{
-    res.status(200).json({
-        message:"All right"
-    })
+    const {userName, emailId, fullName,  password} = req.body;
+    if(userName ==="" || emailId==="" ||  password===""){
+        throw new apiError(409, "All Fields are Required!!");
+    }
+
+    console.log(userName);
+    
+    
 })
 
 
